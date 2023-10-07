7b8862a52ea4d6b59350418bfb73413ba2dd1b37
Ajaykumar <ajaykumar931396@gmail.com>
2023-10-07 02:07:45 -0700

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,106 @@
+//  java sceipt consol API application  programming interface
+// console.log("hello world");
+// console.warn("this is warnig");
+// console.error("this is arror")
+// console.clear()
+// console.assert(4 == 9 - 5);
+// console.error("this is arror")
+// alert("jay swaminarayan");
+// document.write("hii ajay this  is  doc.write")
+// var ajay = 100;
+// var m = 1000;
+// Number=ajay+m;
+// console.log(Number)
+// console.log(m + ajay);
+// var marks = {
+// ajay: 100,
+// meena: 789,
+// bhadresh: 177
+// }
+// console.log(marks);
+
+// console.log(!true);
+// console.log(!false);
+// a=1000;
+// b=4904;
+
+// function avg(a,b){
+//     return a+b;
+// }
+
+// console.log(avg(100,238))
+// console.log(avg(239478,2349288))
+// console.log(avg(10342830,238423))
+// if(234>23){
+//     console.log("ths is  alwatserm rdf");
+// }
+
+
+// age=1;
+// if (age>20){
+//     console.log("you are not kid")
+// }
+
+// else{
+//     age=100;
+//     console.log("you are a kidd")
+//     console.log(age)
+//     age=1
+// }
+// console.log(age)
+
+
+
+
+// arr.forEach(function(element){
+//     console.log(element)
+// })
+
+// arr.forEach(function(element){
+//     console.log(element)
+// })
+
+// let ajay
+// const ajay2=100
+
+// j=0;
+// while(j<arr.length){
+//     console.log(arr[j])
+//     j++;
+// }
+// k=0;
+// do {
+//     console.log(arr[k])
+//     k++
+// } while (k<0);
+
+// var arr = [1, 3, 4, 5, 6, 7, 88, 9, 0]
+// console.log(arr);
+// for (var i = 0; i < arr.length; i++) {
+
+//     console.log(arr[i])
+// }
+
+
+// let myarr=["ajay","harry","vishal","bhadresh",100];
+// console.log(myarr);
+// console.log(myarr.length);  
+// let mydate=new Date();
+// console.log(mydate)
+// console.log(mydate.getDate())
+// console.log(mydate.getDay())
+// console.log(mydate.getFullYear())
+// console.log(mydate.getMonth())
+// console.log(mydate.getTime())
+
+
+
+// document object model---<DOM
+     
+
+let elem=document.getElementById('click');
+console.log(elem);
+let elemclass=document.getElementsByClassName("container1");
+console.log(elemclass);
+elemclass[0].style.background="red"
+
